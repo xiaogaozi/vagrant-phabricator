fe5e47645dd8d142bbcdaddbe5297e58915f071d
Phil Frost <pfrost@macprofessionals.com>
2014-02-26 11:07:16 -0500

    Use magic geolocating Debian mirror
    The previous mirror is quite slow, for people outside of China. This one should
    auto-select a mirror that's geographically close to anyone using it.

--- manifests/default.pp
+++ manifests/default.pp
@@ -6,13 +6,13 @@ class { 'apt':
 }
 
 apt::source { 'debian':
-  location    => 'http://mirrors.163.com/debian/',
+  location    => 'http://http.debian.net/debian/',
   repos       => 'main contrib non-free',
   include_src => false
 }
 
 apt::source { 'debian_updates':
-  location    => 'http://mirrors.163.com/debian/',
+  location    => 'http://http.debian.net/debian/',
   release     => "${::lsbdistcodename}-proposed-updates",
   repos       => 'main contrib non-free',
   include_src => false
